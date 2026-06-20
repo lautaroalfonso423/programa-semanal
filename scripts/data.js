e96f1da90224056b88745ea64eca3016595f6f2f
Lautaro Alfonso <sebastiansosa3011@gmail.com>
2026-06-20 20:01:29 -0300

    primeros cambios

---
+++ scripts/data.js
@@ -0,0 +1,238 @@
+const itinerarios = {
+    ////// Sur Barranqueras
+    "ELJY-6Z3Y-N3Q3": [
+        { 
+            title: "Programa de actividades para la Congregación Sur Barranqueras", 
+            data: [
+                {
+                dia:"8 de Julio",
+                actividades:[
+                    {nombre:"Predicación", hora: "09:30", lugar:"Iglesia Central"},
+                    {nombre:"Predicación", hora: "06:30", lugar:"Iglesia Central"}
+                ]
+                },
+                {
+                dia:"8 de Julio",
+                actividades:[
+                    {nombre:"Predicación", hora: "06:30", lugar:"Iglesia Central"}
+                ]
+                }
+            ],
+            contactos: [
+                {
+                    encargado: "Gustavo Correa",
+                    numero: "+54 9 362 456-8260"
+                },
+                {
+                    encargado: "Joel Juarez",
+                    numero: "+54 9 2604 80-4926"
+                }
+            ]
+        }
+        
+    ],
+    ////// Norte Barranqueras
+    "ABCD-1234-XYZ9": [
+         { 
+            title: "Programa de actividades para la Congregación Sur Resistencia", 
+            data: [
+                {
+                dia:"8 de Julio",
+                actividades:[
+                    {nombre:"Predicación", hora: "09:30", lugar:"Iglesia Central"},
+                    {nombre:"Predicación", hora: "06:30", lugar:"Iglesia Central"}
+                ]
+                },
+                {
+                dia:"8 de Julio",
+                actividades:[
+                    {nombre:"Predicación", hora: "06:30", lugar:"Iglesia Central"}
+                ]
+                }
+            ],
+            contactos: [
+                {
+                    encargado: "Gustavo Correa",
+                    numero: "+54 362 47878868"
+                },
+                {
+                    encargado: "Gustavo Correa",
+                    numero: "+54 362 47878868"
+                },
+                {
+                    encargado: "Gustavo Correa",
+                    numero: "+54 362 47878868"
+                },
+                {
+                    encargado: "Gustavo Correa",
+                    numero: "+54 362 47878868"
+                },
+
+            ]
+        }
+    ],
+
+    ////// B^ Güiraldes
+    "KLNW-4821-PMX4": [
+         { 
+            title: "Programa de actividades para la Congregación Sur Barranqueras", 
+            data: [
+                {
+                dia:"8 de Julio",
+                actividades:[
+                    {nombre:"Predicación", hora: "09:30", lugar:"Iglesia Central"},
+                    {nombre:"Predicación", hora: "06:30", lugar:"Iglesia Central"}
+                ]
+                },
+                {
+                dia:"8 de Julio",
+                actividades:[
+                    {nombre:"Predicación", hora: "06:30", lugar:"Iglesia Central"}
+                ]
+                }
+            ],
+            contactos: [
+                {
+                    encargado: "Gustavo Correa",
+                    numero: "+54 362 47878868"
+                },
+                {
+                    encargado: "Gustavo Correa",
+                    numero: "+54 362 47878868"
+                },
+                {
+                    encargado: "Gustavo Correa",
+                    numero: "+54 362 47878868"
+                },
+                {
+                    encargado: "Gustavo Correa",
+                    numero: "+54 362 47878868"
+                },
+
+            ]
+        }
+    ],
+
+    ////// Tiro Federal
+    "TRQV-8593-KFB2": [
+         { 
+            title: "Programa de actividades para la Congregación Sur Barranqueras", 
+            data: [
+                {
+                dia:"8 de Julio",
+                actividades:[
+                    {nombre:"Predicación", hora: "09:30", lugar:"Iglesia Central"},
+                    {nombre:"Predicación", hora: "06:30", lugar:"Iglesia Central"}
+                ]
+                },
+                {
+                dia:"8 de Julio",
+                actividades:[
+                    {nombre:"Predicación", hora: "06:30", lugar:"Iglesia Central"}
+                ]
+                }
+            ],
+            contactos: [
+                {
+                    encargado: "Gustavo Correa",
+                    numero: "+54 362 47878868"
+                },
+                {
+                    encargado: "Gustavo Correa",
+                    numero: "+54 362 47878868"
+                },
+                {
+                    encargado: "Gustavo Correa",
+                    numero: "+54 362 47878868"
+                },
+                {
+                    encargado: "Gustavo Correa",
+                    numero: "+54 362 47878868"
+                },
+
+            ]
+        }
+    ],
+
+    ////// Este Resistencia
+    "HBLZ-1064-WRE7": [
+         { 
+            title: "Programa de actividades para la Congregación Sur Barranqueras", 
+            data: [
+                {
+                dia:"8 de Julio",
+                actividades:[
+                    {nombre:"Predicación", hora: "09:30", lugar:"Iglesia Central"},
+                    {nombre:"Predicación", hora: "06:30", lugar:"Iglesia Central"}
+                ]
+                },
+                {
+                dia:"8 de Julio",
+                actividades:[
+                    {nombre:"Predicación", hora: "06:30", lugar:"Iglesia Central"}
+                ]
+                }
+            ],
+            contactos: [
+                {
+                    encargado: "Gustavo Correa",
+                    numero: "+54 362 47878868"
+                },
+                {
+                    encargado: "Gustavo Correa",
+                    numero: "+54 362 47878868"
+                },
+                {
+                    encargado: "Gustavo Correa",
+                    numero: "+54 362 47878868"
+                },
+                {
+                    encargado: "Gustavo Correa",
+                    numero: "+54 362 47878868"
+                },
+
+            ]
+        }
+    ],
+
+    ////// Sur Resistencia
+    "FXMS-7315-NJY3": [
+         { 
+            title: "Programa de actividades para la Congregación Sur Barranqueras", 
+            data: [
+                {
+                dia:"8 de Julio",
+                actividades:[
+                    {nombre:"Predicación", hora: "09:30", lugar:"Iglesia Central"},
+                    {nombre:"Predicación", hora: "06:30", lugar:"Iglesia Central"}
+                ]
+                },
+                {
+                dia:"8 de Julio",
+                actividades:[
+                    {nombre:"Predicación", hora: "06:30", lugar:"Iglesia Central"}
+                ]
+                }
+            ],
+            contactos: [
+                {
+                    encargado: "Gustavo Correa",
+                    numero: "+54 362 47878868"
+                },
+                {
+                    encargado: "Gustavo Correa",
+                    numero: "+54 362 47878868"
+                },
+                {
+                    encargado: "Gustavo Correa",
+                    numero: "+54 362 47878868"
+                },
+                {
+                    encargado: "Gustavo Correa",
+                    numero: "+54 362 47878868"
+                },
+
+            ]
+        }
+    ]
+};
