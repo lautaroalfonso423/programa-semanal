3f54d3bd454725f7e9e1bf151101625cfd637530
Lautaro Alfonso <sebastiansosa3011@gmail.com>
2026-06-24 00:35:12 -0300

    camvbios

--- scripts/entre_semana.js
+++ scripts/entre_semana.js
@@ -1,13 +1,17 @@
 
 async function Tablas(nombre) {
+
     const SHEET_ID = "1uLNHtuM2fcaaCaIXh5lGy-Im5vou6uItiM8YjgiLmL4"
 
     const url =
-    `https://opensheet.elk.sh/${SHEET_ID}/${nombre}`    
+    `https://opensheet.elk.sh/${SHEET_ID}/${nombre}`
+    
+    
     
     try {
     const res = await fetch(url)
-    return await res.json()        
+    
+    return await res.json()     
     } catch (error) {
         throw new Error(error)
     }
@@ -15,74 +19,110 @@ async function Tablas(nombre) {
 }
 
 async function cargarDatos() {
-    
-    const programa = await Tablas("Tabla_Principal")
 
+    const contenedor = document.getElementById("data_entre_semana")
+    if(contenedor){
+
+        contenedor.innerHTML ="<p class='miercoles-cargando'>Cargando la Base de Datos...</p>";
+    }
+
+    
+    try{
+
+    const programa = await Tablas("Tabla_Principal")
     const fila = programa[0]
 
     const tesoros = await Tablas("Tesoros")
-    const maestros = await Tablas( fila.Tabla_Mejores_Maestros)
+    const maestros = await Tablas("Mejores_Maestros")
     const vida_cristiana = await Tablas( fila.Tabla_Vida_Cristiana)
-    try{
+    if (contenedor) {
+    
+        contenedorGeneral.innerHTML = "";
+    }
 
-const tesoros =
-await Tablas("Tesoros")
+    Tesoros(tesoros)
+    
 
-console.log(tesoros)
+    }catch(error){
 
-Tesoros(tesoros)
+    console.log(error)
+    if (contenedor) {
+            contenedorGeneral.innerHTML = "<p class='miercoles-error'>Error al cargar el programa.</p>";
+        }
 
-}catch(error){
-
-console.log(error)
-
-}
+    }
 }
 
 function Tesoros(data){
 
-const contenedor =
-document.getElementById("data_tesoros")
+        const contenedor =
+        document.getElementById("data_tesoros")
 
-if(!contenedor){
-console.log("No existe #data_tesoros")
-return
-}
 
-contenedor.innerHTML=""
+        // contenedor.innerHTML= "<p>Consultando la Base de Datos...</p>"
 
-data.forEach(element=>{
+        data.forEach(element=>{
 
-if(!element.Presidente)
-return
+        if(!element.Presidente)
+        return
 
-contenedor.innerHTML += `
+        contenedor.innerHTML += `
 
-<div class="card">
+        <div class="reunion-tarjeta">
 
-<h3>${element.Presidente ?? ""}</h3>
+            <h3 class="reunion-titulo-principal">
+                <span class="reunion-etiqueta">Fecha</span>${element.Fecha ?? ""}
+            </h3>
 
-<p>${element.Presidente_Sala_B ?? ""}</p>
+            <h3 class="reunion-titulo-principal">
+                <span class="reunion-etiqueta">Presidente:</span>
+                ${element.Presidente ?? ""}
+            </h3>
+            
+            <div class="reunion-fila-info">
+                <span class="reunion-etiqueta">Presidente Sala B:</span>
+                <span class="reunion-asignado">${element.Presidente_Sala_B ?? "-"}</span>
+            </div>
 
-<p>${element.Cantico ?? ""}</p>
+            <div class="reunion-fila-info">
+                <span class="reunion-etiqueta">Cántico:</span>
+                <span class="reunion-asignado">${element.Cantico ?? "-"}</span>
+            </div>
 
-<p>${element.Titulo_Tesoros ?? ""}</p>
+            <div class="reunion-fila-info">
+                <span class="reunion-etiqueta">Título Tesoros:</span>
+                <span class="reunion-asignado">${element.Titulo_Tesoros ?? "-"}</span>
+            </div>
 
-<p>${element.Discursante ?? ""}</p>
+            <div class="reunion-fila-info">
+                <span class="reunion-etiqueta">Discursante:</span>
+                <span class="reunion-asignado">${element.Discursante ?? "-"}</span>
+            </div>
 
-<p>${element.Conductor_Perlas ?? ""}</p>
+            <div class="reunion-fila-info">
+                <span class="reunion-etiqueta">Conductor Perlas:</span>
+                <span class="reunion-asignado">${element.Conductor_Perlas ?? "-"}</span>
+            </div>
 
-<p>${element.Lectura_Biblia ?? ""}</p>
+            <div class="reunion-fila-info">
+                <span class="reunion-etiqueta">Lectura de la Biblia:</span>
+                <span class="reunion-asignado">${element.Lectura_Biblia ?? "-"}</span>
+            </div>
 
-<p>${element.Lector_A ?? ""}</p>
+            <div class="reunion-fila-info">
+                <span class="reunion-etiqueta">Lector A:</span>
+                <span class="reunion-asignado">${element.Lector_A ?? "-"}</span>
+            </div>
 
-<p>${element.Lector_B ?? ""}</p>
+            <div class="reunion-fila-info">
+                <span class="reunion-etiqueta">Lector B:</span>
+                <span class="reunion-asignado">${element.Lector_B ?? "-"}</span>
+            </div>
+        </div>
 
-</div>
+        `
 
-`
-
-})
+        })
 
 }
 
