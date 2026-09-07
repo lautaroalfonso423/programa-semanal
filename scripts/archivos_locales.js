d9a93fdc7c4db26a2be76a9f99d9d825f45f26f7
Lautaro Alfonso <sebastiansosa3011@gmail.com>
2026-09-06 21:12:17 -0300

    cambios

--- scripts/archivos_locales.js
+++ scripts/archivos_locales.js
@@ -25,44 +25,41 @@ window.document.addEventListener("DOMContentLoaded", () => {
     const boton_login = document.getElementById("login_carpeta")
     const boton_fin_de_semana = document.getElementById("fin_semana_carpeta")
     const boton_salida = document.getElementById("salidas_carpeta")
+    // const ruta = "public"
     
     if(boton_entre_semana){
         boton_entre_semana.addEventListener("click", async() => {
-            const ruta = await cargarCarpeta("/reuniones/entre_semana.html")
-            if(!ruta){
-                await cargarCarpeta("public/reuniones/entre_semana.html")
-            } 
+            // await cargarCarpeta("/reuniones/entre_semana.html")
+     
+                await cargarCarpeta(`${ruta}/reuniones/entre_semana.html`)
             EsperaDeDatos()
         })
     }
 
     if(boton_login){
         boton_login.addEventListener("click", async() => {
-            const ruta = await cargarCarpeta("/login.html")
-            if(!ruta){
-                await cargarCarpeta("public/login.html")
-            }
+            // await cargarCarpeta("/login.html")
+         
+                await cargarCarpeta(`${ruta}/login.html`)
+            
         })
     }
 
     if(boton_fin_de_semana){
         boton_fin_de_semana.addEventListener("click", async() => {
-            const ruta = await cargarCarpeta("/reuniones/fin_semana.html")
-            if(!ruta){
-                await cargarCarpeta("public/reuniones/fin_semana.html")
-            }
-            EsperaDeDatosAtalaya()
+                
+                // await cargarCarpeta("/reuniones/fin_semana.html")
+                await cargarCarpeta(`${ruta}/reuniones/fin_semana.html`)
+            
+                EsperaDeDatosAtalaya()
         })
     }
 
     if(boton_salida){
         boton_salida.addEventListener("click", async() => {
 
-            const ruta = await cargarCarpeta("/predicacion/salidas.html");
-            if(!ruta) {
-                await cargarCarpeta("public/predicacion/salidas.html") 
-            } 
-            
+            // await cargarCarpeta("/predicacion/salidas.html");
+            await cargarCarpeta(`${ruta}/predicacion/salidas.html`) 
             EsperaDeDatosSalidas();
         })
     }
--- scripts/predicacion.js
+++ scripts/predicacion.js
@@ -1,10 +1,10 @@
 import { ENV } from "../config.js"
 
-async function TablaSalidas() {
+async function TablaSalidas(nombre) {
 
     const SHEET_ID = ENV.TABLA_PREDICACION
     const url =
-    `https://opensheet.elk.sh/${SHEET_ID}/Tabla_Predi`
+    `https://opensheet.elk.sh/${SHEET_ID}/${nombre}`
     
     try {
         const res = await fetch(url)
@@ -20,15 +20,18 @@ async function TablaSalidas() {
 async function cargarDatosSalidas() {
     
     try {
-        const datos_salidas = await TablaSalidas()
-        if(datos_salidas.length > 0){
-            Salida_Vista(datos_salidas)
         
-        } else {
-            console.warn("No hay ningún programa activo para esta semana.");
+        const datos_salidas_generales = await TablaSalidas("Tabla_General")
+        const datos_salidas_grupales = await TablaSalidas("Tabla_Grupo")
+
+        if(datos_salidas_generales.length > 0 && datos_salidas_grupales.length > 0){
+            Salida_Vista(datos_salidas_generales)
+            Salida_Vista_Grupales(datos_salidas_grupales)
+        }else {
+            console.warn("No hay un programa activo para esta semana.");
             const contenedor = document.getElementById("data_salidas");
             if (contenedor) {
-                contenedor.innerHTML = "<p class='sabado-error'>No hay un programa activo asignado para esta semana.</p>";
+                contenedor.innerHTML = "<p class='sabado-error'>No hay ningún programa activo para esta semana.</p>";
             }
         }
     } catch (error) {
@@ -66,7 +69,7 @@ async function EsperaDeDatosSalidas(){
 
 
 async function Salida_Vista(data) {
-        const contenedor = document.getElementById("contenedor_salidas")
+        const contenedor = document.getElementById("contenedor_salidas_generales")
 
         const dias_de_semana = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sabado", "Domigno"]
 
@@ -88,7 +91,50 @@ async function Salida_Vista(data) {
                         </p>                        
                         <p><strong>Información:</strong> ${element.Dato_Adicional}</p>
                         <p><strong>Conductor:</strong> ${element.Conductor || ""}</p>
-                        <p><strong>Territorio:</strong>  <a href="${element.Link_del_Terrirotorio || ""}" target="_blank" >${element.Territorio || ""}</a></p>
+                        <p><strong>Territorio Principal:</strong>  <a href="${element.Link_del_Terrirotorio || ""}" target="_blank" >${element.Territorio || ""}</a></p>
+                        <p><strong>Territorio Secundario:</strong>  <a target="_blank" >${element.Territorio_Secundario || ""}</a></p>
+                    </div>
+                    ${zoom ? 
+                        `
+                        <div class="evento-footer">
+                        <a href="${element.Link_de_Zoom || ""}" target="_blank" class="btn-zoom">Unirse a Zoom</a>
+                        </div>
+                        `
+                        : ""
+                    } 
+                    
+                </div>
+            `;
+            }
+        });  
+    
+    }
+
+async function Salida_Vista_Grupales(data) {
+        const contenedor = document.getElementById("contenedor_salidas_grupales")
+
+        const dias_de_semana = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sabado", "Domigno"]
+
+
+        data.forEach(element => {
+            if(dias_de_semana.includes(element.Dia)){
+                const zoom = element.Link_de_Zoom; 
+                contenedor.innerHTML += `
+                <div class="tarjeta-evento">
+                    <div class="evento-header">
+                        <span class="etiqueta-dia">${element.Dia || ""}</span>
+                        <span class="etiqueta-hora">${element.Hora || ""}</span>
+                    </div>
+                    <div class="evento-cuerpo">
+                        <p class="dato-ubicacion">
+                            <a href="https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(element.Ubicacion || '')}" target="_blank" class="link-mapa">
+                                <strong>Ubicación:</strong> ${element.Ubicacion || ""}
+                            </a>
+                        </p>                        
+                        <p><strong>Información:</strong> ${element.Dato_Adicional}</p>
+                        <p><strong>Conductor:</strong> ${element.Conductor || ""}</p>
+                        <p><strong>Territorio Principal:</strong>  <a href="${element.Link_del_Terrirotorio || ""}" target="_blank" >${element.Territorio || ""}</a></p>
+                        <p><strong>Territorio Secundario:</strong>  <a target="_blank" >${element.Territorio_Secundario || ""}</a></p>
                     </div>
                     ${zoom ? 
                         `
